7a7531dd449a771657088e873e154d74e2bccfb5
Wanderson M <wandersonmaceds@gmail.com>
2022-12-12 19:30:21 -0300

    task 1: salvando conta no mongodb

--- account/test/use-case/createUserAccount.test.js
+++ account/test/use-case/createUserAccount.test.js
@@ -1,6 +1,6 @@
 import { createUserUseCase } from '../../src/use-case/createUserAccount.js';
 
-const user1 = createUserUseCase('Rita', 'rita@mail.com', 'ritalinda');
-const user2 = createUserUseCase('Josué', 'josue@mail.com', '123bacana');
+const user1 = await createUserUseCase('Rita', 'rita@mail.com', 'ritalinda');
+const user2 = await createUserUseCase('Josué', 'josue@mail.com', '123bacana');
 
 console.log(user1, user2);
