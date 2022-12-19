42e1d7ae50a8f7d71df3c1221eb95935a09dcd4e
Wanderson M <wandersonmaceds@gmail.com>
2022-12-19 20:08:57 -0300

    hashing users password

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -1,12 +1,13 @@
+import { hashPassword } from '../helpers/password.js';
 import { saveAccount } from '../repositories/accountRepository.js';
 
 export async function createUserUseCase(name, email, password) {
     const createdDate = new Date().toISOString().substring(0, 10);
-
+    const hashedPassword = await hashPassword(password);
     const user = {
         name, 
         email,
-        password,
+        password: hashedPassword,
         createdDate
     };
 
